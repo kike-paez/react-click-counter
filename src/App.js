6e66646143145c81f07116fb1dd11ec78a56a4fa
Kike Paez <enrique.paezpozo.com>
2023-05-24 10:14:57 +0200

    Image and general style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import freeCodeCampLogo from './img/freecodecamp-logo.png'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='freecodecamp-logo-container'>
+        <img
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='freeCodeCamp logo'
+        />
+      </div>
+      <div className='main-container'>
+        
+      </div>
     </div>
   );
 }
