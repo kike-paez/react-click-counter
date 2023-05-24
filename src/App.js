ba37fb864d59ee6055110a71f774118940eec03a
Kike Paez <enrique.paezpozo.com>
2023-05-24 10:51:31 +0200

    Added buttons with style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
+import Button from './components/Button.jsx';
 import freeCodeCampLogo from './img/freecodecamp-logo.png'
 
 function App() {
+
+  const handleClick = () => {
+    console.log('Click')
+  }
+
+  const resetCounter = () => {
+    console.log('Reset')
+  }
+
   return (
     <div className="App">
       <div className='freecodecamp-logo-container'>
         <img
           className='freecodecamp-logo'
           src={freeCodeCampLogo}
-          alt='freeCodeCamp logo'
-        />
+          alt='freeCodeCamp logo' />
       </div>
       <div className='main-container'>
-        
+        <Button
+          text='Click'
+          clickButton={true}
+          handleClick={handleClick} />
+        <Button
+          text='Reset'
+          clickButton={false}
+          handleClick={resetCounter} />
       </div>
     </div>
   );
